eeed0dbc27f08ba5c2fc777e065f925b70e72e41
Lnola <nola.luka@gmail.com>
2020-07-03 01:42:00 +0200

    Fixed break if bookmarks are null

--- marvel-heroes/src/utils/addEmptyCharacters.js
+++ marvel-heroes/src/utils/addEmptyCharacters.js
@@ -1,10 +1,12 @@
 export const addEmptyCharacters = (array) => {
-  const amountToAdd = 4 - (array.length % 4);
+  if (array !== null) {
+    const amountToAdd = 4 - (array.length % 4);
 
-  if (amountToAdd % 4 !== 0)
-    for (let i = 0; i < amountToAdd; i++) {
-      array.push(null);
-    }
+    if (amountToAdd % 4 !== 0)
+      for (let i = 0; i < amountToAdd; i++) {
+        array.push(null);
+      }
+  }
 
   return array;
 };
